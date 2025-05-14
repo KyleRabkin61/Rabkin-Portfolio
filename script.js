be36323015db5d6d257a4f7e5b83818bc045c7ef
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-05-14 18:17:24 -0400

    Started implementing GSAP

--- script.js
+++ script.js
@@ -1,44 +1,15 @@
-// let nav = document.querySelector(".navbar");
-// let navHeight = parseInt(getComputedStyle(nav).height, 10);
+gsap.to('h1', {
+    scale: 0.5
+})
 
-// // Add scroll event listener
-// window.addEventListener("scroll", () => {
-//     if (window.scrollY > height - (height * 0.8)) {
-//         nav.classList.add("navbar-scrolled");
-//     } else {
-//         nav.classList.remove("navbar-scrolled");
-//     }
-//     // Update navbar height dynamically
-//     if (nav) {
-//         navHeight = parseInt(getComputedStyle(nav).height, 10);
-//     }
-// });
+gsap.from('h1', {
+    y: '500',
+    ease: 'power1.in',
+    scale: 0.5,
+    duration: 2,
+    delay:1
+})
 
-// // Button on scroll function that does not cut off div
-// function scrollToSection(id) {
-//     const element = document.getElementById(id);
-//     const offset = navHeight;
-//     const elementPosition = element.getBoundingClientRect().top + window.scrollY;
-//     window.scrollTo({
-//         top: elementPosition - offset,
-//         behavior: "smooth"
-//     });
-// }
+const timeline = gsap.timeline();
 
-// // Navbar collapses on click
-// document.addEventListener("DOMContentLoaded", function () {
-//     const navLinks = document.querySelectorAll(".nav-link");
-//     const navbarCollapse = document.querySelector(".navbar-collapse");
-
-//     navLinks.forEach(link => {
-//         link.addEventListener("click", function () {
-//             if (navbarCollapse.classList.contains("show")) {
-//                 if (typeof bootstrap !== "undefined") {
-//                     new bootstrap.Collapse(navbarCollapse, {
-//                         toggle: true
-//                     });
-//                 }
-//             }
-//         });
-//     });
-// });
+timeline.to()
