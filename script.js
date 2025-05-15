aff98755ed3b2d422fa63dcb32f7a320e56c3c49
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-05-15 00:24:05 -0400

    Added navbar + explore section

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+let nav = document.querySelector(".navbar");
+let navHeight = parseInt(getComputedStyle(nav).height, 10);
+
+// Add scroll event listener
+window.addEventListener("scroll", () => {
+    if (window.scrollY > height - (height * 0.8)) {
+        nav.classList.add("navbar-scrolled");
+    } else {
+        nav.classList.remove("navbar-scrolled");
+    }
+    // Update navbar height dynamically
+    if (nav) {
+        navHeight = parseInt(getComputedStyle(nav).height, 10);
+    }
+});
+
+// Button on scroll function that does not cut off div
+function scrollToSection(id) {
+    const element = document.getElementById(id);
+    const offset = navHeight;
+    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({
+        top: elementPosition - offset,
+        behavior: "smooth"
+    });
+}
+
+// Navbar collapses on click
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            if (navbarCollapse.classList.contains("show")) {
+                if (typeof bootstrap !== "undefined") {
+                    new bootstrap.Collapse(navbarCollapse, {
+                        toggle: true
+                    });
+                }
+            }
+        });
+    });
+});
+
 gsap.from('.circle-shape', {
     scale: 0.5,
     rotation:360, 
@@ -60,4 +105,10 @@ gsap.from('.block-6', {
     ease: 'power1.inOut',
     duration: 2,
     delay: 0.5
+})
+
+gsap.to(['.navbar', '.explore'], {
+    duration: 1,
+    delay: 2.5,
+    opacity: 1
 })
