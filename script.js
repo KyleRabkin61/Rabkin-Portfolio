54cd56933d2a7443067bffae20f9bb34a36a1ae4
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-05-15 00:10:39 -0400

    Created landing page animation

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
-gsap.to('h1', {
-    scale: 0.5
-})
-
-gsap.from('h1', {
-    y: '500',
-    ease: 'power1.in',
+gsap.from('.circle-shape', {
     scale: 0.5,
+    rotation:360, 
+    transformOrigin:"50% 50%",
+    ease: 'power1.inOut',
     duration: 2,
-    delay:1
+    delay: 0.5
 })
 
-const timeline = gsap.timeline();
-
-timeline.to()
+gsap.from('.block-1', {
+    x: '-11vw',
+    y: '-9vw',
+    scale: "4.4",
+    opacity: 0.5,
+    ease: 'power1.inOut',
+    duration: 2,
+    delay: 0.5
+})
+gsap.from('.block-2', {
+    x: '15vw',
+    y: '15vw',
+    scale: "3.1",
+    opacity: 0.5,
+    ease: 'power1.inOut',
+    duration: 2,
+    delay: 0.5
+})
+gsap.from('.block-3', {
+    x: "11vw",
+    y: "-9vw",
+    scale: "3.3",
+    opacity: 0.5,
+    ease: 'power1.inOut',
+    duration: 2,
+    delay: 0.5
+})
+gsap.from('.block-4', {
+    x: "-11vw",
+    y: "10vw",
+    scale: "2.4",
+    opacity: 0.5,
+    ease: 'power1.inOut',
+    duration: 2,
+    delay: 0.5
+})
+gsap.from('.block-5', {
+    x: "-2vw",
+    y: "-16vw",
+    scale: "2.4",
+    opacity: 0.5,
+    ease: 'power1.inOut',
+    duration: 2,
+    delay: 0.5
+})
+gsap.from('.block-6', {
+    x: "-2vw",
+    y: "15vw",
+    scale: "2.4",
+    opacity: 0.5,
+    ease: 'power1.inOut',
+    duration: 2,
+    delay: 0.5
+})
